91df67f5335184d91e2bd169cccfb5ec8641589c
regretevator638-lab <regretevator638@gmail.com>
2026-05-27 14:03:09 +0700

    Update FPS.lua

--- FPS.lua
+++ FPS.lua
@@ -15,8 +15,8 @@
 local CONFIG = {
 
     -- [ ข้อมูลทั่วไป ]
-    HUB_NAME    = "UG Hub SEX",               -- ชื่อโปรแกรม
-    VERSION     = "vกูไม่บอกไอ้ เหี้ย",                 -- เวอร์ชั่น
+    HUB_NAME    = "UG Hub",               -- ชื่อโปรแกรม
+    VERSION     = "v6.2",                 -- เวอร์ชั่น
     AUTHOR      = "regretevator638",       -- ชื่อผู้สร้าง
     DISCORD     = "discord.gg/v6Qh69hqd", -- ลิงค์ Discord
 
